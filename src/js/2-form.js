2bedcbb81021243a242230e9b7c7e1d7d04c002e
shepel30 <oleksandra.alymova@gmail.com>
2024-04-05 22:32:23 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('.feedback-form');
 
-
     const saveFormDataToLocalStorage = () => {
         const formData = {
-            email: form.email.value,
-            message: form.message.value
+            email: form.email.value.trim(),
+            message: form.message.value.trim()
         };
         localStorage.setItem('feedback-form-state', JSON.stringify(formData));
     };
-
 
     const loadFormDataFromLocalStorage = () => {
         const savedData = localStorage.getItem('feedback-form-state');
@@ -20,12 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-
     loadFormDataFromLocalStorage();
 
-
     form.addEventListener('input', saveFormDataToLocalStorage);
-
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
